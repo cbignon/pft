02a9af27ac35fa645b5ea52f286d10fb0893e1c2
cbignon <71795678+cbignon@users.noreply.github.com>
2021-04-01 12:37:32 +0200

    fix 0%

--- ft_parse_format.c
+++ ft_parse_format.c
@@ -6,7 +6,7 @@
 /*   By: cbignon <cbignon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/12 10:28:50 by cbignon           #+#    #+#             */
-/*   Updated: 2021/03/31 16:51:10 by cbignon          ###   ########.fr       */
+/*   Updated: 2021/04/01 12:36:52 by cbignon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,6 @@ void	ft_parse_format(const char *fmt, t_flags *flags)
 			flags->type = fmt[flags->i];
 			flags->i++;
 			if (flags->minus == 1 && flags->zero == 1)
-				flags->zero = 0;
-			if (flags->precision > 0 && flags->zero == 1)
 				flags->zero = 0;
 			return ;
 		}
